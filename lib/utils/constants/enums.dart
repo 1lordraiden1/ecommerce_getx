fd6769199e948faa14192f852b5a30e4e3c59e51
lordraiden <lordraiden101@gmail.com>
2023-11-08 16:08:41 +0200

    texts and images added

---
+++ lib/utils/constants/enums.dart
@@ -0,0 +1,15 @@
+enum TextSizes { small, medium, large }
+
+enum OrderStatus { processing, shipped, delivered }
+
+enum PaymentMethods {
+  paypal,
+  googlePay,
+  applePay,
+  visa,
+  masterCard,
+  creditCard,
+  paystack,
+  razorPay,
+  paytm,
+}
---
+++ lib/utils/constants/text_strings.dart
@@ -0,0 +1,18 @@
+class XTexts {
+  // -- On Boarding Texts
+  static const String onBoardingTitle1 = 'Choose your product';
+  static const String onBoardingTitle2 = 'Select Payment Method';
+  static const String onBoardingTitle3 = 'Deliver at your door step';
+
+  static const String onBoardingSubTitle1 =
+      'Welcome to a World of Limitless Choices - Your Perfect Product Awaits!';
+  static const String onBoardingSubTitle2 =
+      'For Seamless Transactions, Choose Your Payment Path - Your Convenience, Our Priority!';
+  static const String onBoardingSubTitle3 =
+      'From Our Doorstep to Yours - Swift, Secure, and Contactless Delivery!';
+
+  // -- Home
+
+  static const String homeAppbarTitle = 'Good day for shopping';
+  static const String homeAppbarSubTitle = 'Xtreme';
+}
