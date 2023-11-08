9a540ab3d9f3b7f2f1dea3591406f545a242ee04
lordraiden <lordraiden101@gmail.com>
2023-11-08 15:53:15 +0200

    sizes

---
+++ lib/utils/constants/image_strings.dart
@@ -0,0 +1,12 @@
+class XImages {
+  XImages._();
+
+  // -- App Logos
+  static const String darkAppLogo = "";
+  static const String lightAppLogo = "";
+
+  // -- Social Logos
+  static const String google = "";
+  static const String facebook = "";
+  
+}
