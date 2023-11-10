113b49fa38437f438b79302a6de5c2efc49e1a57
lordraiden <lordraiden101@gmail.com>
2023-11-10 16:44:52 +0200

    init device

---
+++ lib/utils/device/device_utility.dart
@@ -0,0 +1,119 @@
+import 'dart:ffi';
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:url_launcher/url_launcher_string.dart';
+
+class XDeviceUtils {
+  static void hideKeyboard(BuildContext context) {
+    FocusScope.of(context).requestFocus(FocusNode());
+  }
+
+  static Future<void> setStateBarColor(Color color) async {
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(statusBarColor: color),
+    );
+  }
+
+  static bool isLandscapeOrientation(BuildContext context) {
+    final viewInsets = View.of(context).viewInsets;
+    return viewInsets.bottom == 0;
+  }
+
+  static bool isPortraitOrientation(BuildContext context) {
+    final viewInsets = View.of(context).viewInsets;
+    return viewInsets.bottom != 0;
+  }
+
+  static void setFullScreen(bool enable) {
+    SystemChrome.setEnabledSystemUIMode(
+      enable ? SystemUiMode.immersiveSticky : SystemUiMode.edgeToEdge,
+    );
+  }
+
+  static double getScreenHeight(BuildContext context) {
+    return MediaQuery.of(context).size.height;
+  }
+
+  static double getScreenWidth(BuildContext context) {
+    return MediaQuery.of(context).size.width;
+  }
+
+  static double getPixelRatio() {
+    return MediaQuery.of(Get.context!).devicePixelRatio;
+  }
+
+  static double getStatusBarHeight() {
+    return MediaQuery.of(Get.context!).padding.top;
+  }
+
+  static double getNavigationBarHeight() {
+    return kBottomNavigationBarHeight;
+  }
+
+  static double getAppBarHeight() {
+    return kToolbarHeight;
+  }
+
+  static Future<bool> isKeyboardVisible() async {
+    final viewInsets = MediaQuery.of(Get.context!).viewInsets;
+    return viewInsets.bottom > 0;
+  }
+
+  static double getKeyboardHeight(BuildContext context) {
+    final viewInsets = View.of(context).viewInsets;
+    return viewInsets.bottom;
+  }
+
+  static Future<bool> isPhysicalDevice() async {
+    return defaultTargetPlatform == TargetPlatform.android ||
+        defaultTargetPlatform == TargetPlatform.iOS;
+  }
+
+  static void vibrate(Duration duration) {
+    HapticFeedback.vibrate();
+    Future.delayed(duration, () => HapticFeedback.vibrate());
+  }
+
+  static Future<void> setPreferredOrientations(
+      List<DeviceOrientation> orientations) async {
+    await SystemChrome.setPreferredOrientations(orientations);
+  }
+
+  static void hideStatusBar(bool enable) {
+    SystemChrome.setEnabledSystemUIMode(
+      SystemUiMode.manual,
+      overlays: SystemUiOverlay.values,
+    );
+  }
+
+  static Future<bool> hasInternetConnection() async {
+    try {
+      final result = await InternetAddress.lookup('example.com');
+      return result.isNotEmpty && result[0].rawAddress.isNotEmpty;
+    } on SocketException catch (_) {
+      return false;
+    }
+  }
+
+  static bool isIOS() {
+    return Platform.isIOS;
+  }
+
+  static bool isAndroid() {
+    return Platform.isAndroid;
+  }
+
+  static void launcherUrl(String url) async {
+    if (await canLaunchUrlString(url)) {
+      await launchUrlString(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+}
