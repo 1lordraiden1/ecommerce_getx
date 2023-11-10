c3b2916dc5f2f632a51e0c52733fe005db10e90a
lordraiden <lordraiden101@gmail.com>
2023-11-10 16:10:50 +0200

    added some packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:xstore/app.dart';
 
 
-import 'app.dart';
+
 
 void main() {
   runApp(const MyApp());
---
+++ lib/utils/constants/api_constants.dart
@@ -0,0 +1,3 @@
+class APIConstants {
+  static const String tSecretAPIKey = "";
+}
