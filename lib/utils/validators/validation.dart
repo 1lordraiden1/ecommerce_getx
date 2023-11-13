acc74947b5be8e39615f001d3e2954e17d45a355
lordraiden <lordraiden101@gmail.com>
2023-11-13 09:03:25 +0200

    utils finished successfully

---
+++ lib/utils/validators/validation.dart
@@ -0,0 +1,53 @@
+class XValidator {
+  static String? validateEmail(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Email is required';
+    }
+
+    final emailRegExp = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
+
+    if (!emailRegExp.hasMatch(value)) {
+      return 'Invalid email address';
+    }
+
+    return null;
+  }
+
+  static String? validatePassword(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Password is required';
+    }
+
+    if (value.length < 6) {
+      return 'Password must be at least 6';
+    }
+
+    if (!value.contains(RegExp(r'[A-Z]'))) {
+      return 'Password must contain at least one uppercase letter';
+    }
+
+    if (!value.contains(RegExp(r'[0-9]'))) {
+      return 'Password must contain at least one number';
+    }
+
+    if (!value.contains(RegExp(r'[!@#$%^&*(),.?":{}|<>]'))) {
+      return 'Password must contain at least one special character';
+    }
+
+    return null;
+  }
+
+   static String? validatePhoneNumber(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Phone number is required';
+    }
+
+    final PhoneRegExp = RegExp(r'^\d{10}$');
+
+    if (!PhoneRegExp.hasMatch(value)) {
+      return 'Invalid Phone number (10 digits required)';
+    }
+
+    return null;
+  }
+}
